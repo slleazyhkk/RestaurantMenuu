8e0e0846a695675e296d8f18059baaf49fd09630
slleazyhkk <m2408026@edu.misis.ru>
2025-05-31 16:51:40 +0300

    Update Cafe.cs

--- Model/Core/Est/Cafe.cs
+++ Model/Core/Est/Cafe.cs
@@ -1,12 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model.Core
+namespace Model.Core.Est
 {
-    internal class Сфау
+    public class Cafe : Establishment
     {
+        public Cafe(string name, string address, string phone) : base(name, address, phone) { }
+        public override string GetEstablishmentType()
+        {
+            return "Кафе";
+        }
     }
 }
