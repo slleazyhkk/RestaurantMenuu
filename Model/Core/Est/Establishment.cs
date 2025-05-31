356fab3e31329d106acb47841234330ae7a1f4a7
slleazyhkk <m2408026@edu.misis.ru>
2025-05-31 16:52:38 +0300

    Update Establishment.cs

--- Model/Core/Est/Establishment.cs
+++ Model/Core/Est/Establishment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Model.Core.Dishes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,38 @@ using System.Threading.Tasks;
 
 namespace Model.Core.Est
 {
-    public abstract partial class Establishment
+    public abstract partial class Establishment:ISeasonMenu
     {
         public string Name { get; set; }
         public string Address { get; set; }
         public string AdminPhone { get; set; }
-        public ChangeMenu Menu { get; set; }
+        public IMenu RegularMenu { get; set; }
+        public IMenu SeasonMenu { get; set; }
+        public bool HasSeasonMenu=>SeasonMenu!=null&& SeasonMenu.Dishes.Any();
 
-        protected Establishment(string name, string address, string adminPhone, ChangeMenu menu)
+        protected Establishment(string name, string address, string adminPhone)
         {
             Name = name;
             Address = address;
             AdminPhone = adminPhone;
-            Menu = new ChangeMenu("Меню");
+            RegularMenu = new ChangeMenu();
+            SeasonMenu = new ChangeMenu();
         }
         public abstract string GetEstablishmentType();
+
+        public void AddSeasonDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSeasonDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearSeasonMenu()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
